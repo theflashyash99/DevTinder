ab23dc11cffaefb0ca5a0127284fd218f1878032
theflashyash99 <theflashyash99@gmail.com>
2025-08-24 15:00:02 +0530

    comment correction

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 
-// the best way to connect the Mongoose to nodeJS is to make a async function and then connect by await it. as mongoose.connect return a promise when we execute the function the function will return a promise that we'll be handelled by then and catch.
+// the best way to connect the Mongoose with nodeJS is to make a async function and then connect by await it. as mongoose.connect return a promise when we execute the function the function will return a promise that we'll be handelled by then and catch.
 
 const connectDb = async () => {
   await mongoose.connect(
