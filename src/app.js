cdc1d354b55cf05e56e4585a3263116f2da4b492
theflashyash99 <theflashyash99@gmail.com>
2025-08-25 14:53:17 +0530

    dynamic data user by email

--- src/app.js
+++ src/app.js
@@ -3,29 +3,46 @@ const connectDb = require("./config/database");
 const app = express();
 const User = require("../models/user");
 
-
-app.use(express.json())
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  console.log(req.body)
+
+  console.log(req.body);
   //creating a new instance of User model.
-//   const user = new User({
-//     firstName: "Rashid ",
-//     lastName: "Ali",
-//     email: "Rashid@gmail.com",
-//     password: "123456789",
-//     age: 21,
-//   });
-//   try {
-//     await user.save();
-//     res.send("User added successfuly");
-//   } catch (err) {
-//     res.send("Problem with adding User:" + err.message);
-//   }
+  //----------------------Manual & Hard-coded data passing
+    // const user = new User({
+    //   firstName: "Rashid ",
+    //   lastName: "Ali",
+    //   email: "Rashid@gmail.com",
+    //   password: "123456789",
+    //   age: 21,
+    // });
 
-
-
+    //------------------------------The Dyanamic data transfer---------------------------------------
+    //creating a new instance of User model.
+    const user = new User(req.body);
+    try {
+      await user.save();
+      res.send("User added successfuly");
+    } catch (err) {
+      res.send("Problem with adding User:" + err.message);
+    }
 });
+
+app.get("/feed",async(req,res)=>{
+  const userEmail = req.body.email;
+ try{
+  const users =  await User.find({email : userEmail});
+  if(users.length === 0 ){
+    res.status(404).send("User not Found");
+  }else{
+    res.send(users)
+  }
+} catch(err){
+  console.error("Something went wrong!!!");
+}
+
+})
 
 connectDb()
   .then(() => {
