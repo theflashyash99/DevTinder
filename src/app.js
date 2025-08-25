7654d972f11d478d952142bb8600c27bed39e2da
theflashyash99 <theflashyash99@gmail.com>
2025-08-25 15:01:52 +0530

    API call to get all user from the DB

--- src/app.js
+++ src/app.js
@@ -6,44 +6,55 @@ const User = require("../models/user");
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-
   console.log(req.body);
   //creating a new instance of User model.
   //----------------------Manual & Hard-coded data passing
-    // const user = new User({
-    //   firstName: "Rashid ",
-    //   lastName: "Ali",
-    //   email: "Rashid@gmail.com",
-    //   password: "123456789",
-    //   age: 21,
-    // });
+  // const user = new User({
+  //   firstName: "Rashid ",
+  //   lastName: "Ali",
+  //   email: "Rashid@gmail.com",
+  //   password: "123456789",
+  //   age: 21,
+  // });
 
-    //------------------------------The Dyanamic data transfer---------------------------------------
-    //creating a new instance of User model.
-    const user = new User(req.body);
-    try {
-      await user.save();
-      res.send("User added successfuly");
-    } catch (err) {
-      res.send("Problem with adding User:" + err.message);
-    }
+  //------------------------------The Dyanamic data transfer---------------------------------------
+  //creating a new instance of User model.
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("User added successfuly");
+  } catch (err) {
+    res.send("Problem with adding User:" + err.message);
+  }
 });
 
-app.get("/feed",async(req,res)=>{
+//Finding the user based on email condition.
+app.get("/feed", async (req, res) => {
   const userEmail = req.body.email;
- try{
-  const users =  await User.find({email : userEmail});
-  if(users.length === 0 ){
-    res.status(404).send("User not Found");
-  }else{
-    res.send(users)
+  try {
+    const users = await User.find({ email: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not Found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    console.error("Something went wrong!!!");
   }
-} catch(err){
-  console.error("Something went wrong!!!");
-}
-
-})
-
+});
+// finding all the user in the query DB.
+app.get("/user", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("User not found!!!");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong!!!");
+  }
+});
 connectDb()
   .then(() => {
     console.log("Database connection established.........");
