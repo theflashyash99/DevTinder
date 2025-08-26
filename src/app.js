330001837bf7f14a77a1a3109eb7f8a689cf1d33
theflashyash99 <theflashyash99@gmail.com>
2025-08-26 13:42:54 +0530

    Updated  API

--- src/app.js
+++ src/app.js
@@ -80,6 +80,20 @@ app.delete("/user", async (req, res) => {
   }
 });
 
+ // update a user---------------------------------
+
+ app.patch("/user",async (req,res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+     const updatedUser = await User.findByIdAndUpdate({_id : userId}, data,{returnDocument : "after"});
+     console.log(updatedUser);
+     res.send("User's data has been updated!!!")
+  } catch (err) {
+    res.status(400).send("Something went wrong!!!");
+  }
+ });
+
 connectDb()
   .then(() => {
     console.log("Database connection established.........");
