221260f1ce38fdae062ead950b767659d8b490fd
theflashyash99 <theflashyash99@gmail.com>
2025-08-28 14:47:17 +0530

    exploring the validation String and Number

--- models/user.js
+++ models/user.js
@@ -3,26 +3,39 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    required : true,  
+    required: true,
+    minLength : 4,
   },
   lastName: {
     type: String,
   },
   email: {
     type: String,
-    required : true,
-    unique : true,
-  
+    required: true,
+    unique: true,
+    lowercase : true,
+    trim : true,
   },
   password: {
     type: String,
-    
   },
   age: {
     type: Number,
+    min : 18,
+    max : 50,
   },
   gender: {
     type: String,
+  },
+  photoURL: {
+    type: String,
+  },
+  about: {
+    type: String,
+    default: "This is the default value of user",
+  },
+  skills: {
+    type: [String], // it will require a sting of arrays thats why we've given it the array.
   },
 });
 
