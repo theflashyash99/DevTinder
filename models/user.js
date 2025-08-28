95f67c5fae2c5920d83f4aa00c5b39ebb5bdddb6
theflashyash99 <theflashyash99@gmail.com>
2025-08-28 13:56:36 +0530

    exploring the validation {unique key}

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    required : true, 
+    required : true,  
   },
   lastName: {
     type: String,
@@ -11,6 +11,7 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required : true,
+    unique : true,
   
   },
   password: {
