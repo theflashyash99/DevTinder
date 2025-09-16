96b4aface8886d9153f63bd7a3e4b9cf8d4f997a
theflashyash99 <theflashyash99@gmail.com>
2025-09-16 15:07:20 +0530

    Email validator in API validation

--- models/user.js
+++ models/user.js
@@ -1,55 +1,63 @@
 const mongoose = require("mongoose");
-
+const validator = require("validator");
 //instance of new schema made so use new.
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true, 
-    minLength: 4, // for string minlength
-  },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true, // it will make sure every email is unique.
-    lowercase: true, //  it will make string into lower case.
-    trim: true, // it will remove white space.
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-    min: 18,
-    max: 50,
-  },
-  gender: {
-    type: String,
-    //custom validation by validate function.
-    validate(value){
-      if (!["male", "female", "others"].includes(value)) {
-        throw new Error("Gender data is not valid");
-      }
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4, // for string minlength
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true, // it will make sure every email is unique.
+      lowercase: true, //  it will make string into lower case.
+      trim: true, // it will remove white space.
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("The email is invalid");
+        }
+      },
+    },
+    password: {
+      type: String,
+    },
+    age: {
+      type: Number,
+      min: 18,
+      max: 50,
+    },
+    gender: {
+      type: String,
+      //custom validation by validate function.
+      // value means that the given value in the field.
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoURL: {
+      type: String,
+      default:
+        "https://www.pnrao.com/wp-content/uploads/2023/06/dummy-user-male.jpg",
+    },
+    about: {
+      type: String,
+      default: "This is the default value of user",
+    },
+    skills: {
+      type: [String], // it will require a sting of arrays thats why we've given it the array.
     },
   },
-  photoURL: {
-    type: String,
-    default : "https://www.pnrao.com/wp-content/uploads/2023/06/dummy-user-male.jpg",
-  },
-  about: {
-    type: String,
-    default: "This is the default value of user",
-  },
-  skills: {
-    type: [String], // it will require a sting of arrays thats why we've given it the array.
-  },
-  
-},
-{
-    timestamps:true,
-  });
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
