27d0b11936898dc9e35b89d8474e8b3b35f65271
theflashyash99 <theflashyash99@gmail.com>
2025-09-28 11:18:42 +0530

    sending the cookies

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,28 +1,4 @@
-const authAdmin = (req, res, next) => {
-  console.log("Admin auth is being checked !!!");
-  const token = "xyz";
-  const isAdminAuthenticated = token === "xyz";
-  // if it will not match it will not pass the auth.
-
-  if (!isAdminAuthenticated) {
-    res.status(401).send("Unauthorized request !!!");
-  } else {
-    next();
-  }
-};
-
-const userAuth = (req, res, next) => {
-  console.log("User auth is being checked !!!");
-  const token = "xyz";
-  const isAdminAuthenticated = token === "xyz";
-
-  if (!isAdminAuthenticated) {
-    res.status(401).send("Unauthorized request !!!");
-  } else {
-    next();
-  }
-};
+const userAuth = (req, res, next) => {};
 module.exports = {
-  authAdmin,
   userAuth,
 };
