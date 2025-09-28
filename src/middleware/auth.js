913943be57574578c84044680d7e58e7b5288868
theflashyash99 <theflashyash99@gmail.com>
2025-09-28 12:02:44 +0530

    auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,4 +1,31 @@
-const userAuth = (req, res, next) => {};
+const jwt = require("jsonwebtoken");
+const userAuth = async (req, res, next) => {
+// Read the token from the req cookies.
+ const cookies = req.cookies;
+ const {token} = cookies;
+ if(!token){
+  throw new Error ("The token not available");
+ }
+
+ const decordedData = jwt.verify(token,"DEV@Tinder$790");
+  
+ const {id} = decordedData;
+
+ const user = await User.findOne({_id: id});
+if(!user){
+  throw new Error ("User does not exist in Database!!!")
+
+}
+
+res.send(user);
+
+
+// Validate the token
+// find the user in DB
+
+
+
+};
 module.exports = {
   userAuth,
 };
