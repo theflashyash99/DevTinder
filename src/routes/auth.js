23f88e96c58fdfd57646443d3db09fbb66f2e827
theflashyash99 <theflashyash99@gmail.com>
2025-10-14 14:13:55 +0530

    logout API

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -81,8 +81,10 @@ authRouter.post("/login", async (req, res) => {
 });
 
 authRouter.post("/logout",async (req,res) => {
-
-
+  res.cookie("token",null,{
+    expires:  new Date(Date.now()),
+  });
+  res.send("Logout Successfully!!!");
 });
 
 module.exports = authRouter;
