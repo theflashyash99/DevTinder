6ae0388482178ce3264a8d607a606197ce2eb5f8
theflashyash99 <theflashyash99@gmail.com>
2025-12-06 10:47:40 +0530

    profile update from the user

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,20 +1,41 @@
 const express = require("express");
 const requestRouter = express.Router();
 const { userAuth } = require("../middleware/auth");
+const { ConnectionRequestModel } = require("../../models/connectionRequest");
 
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    //sending connection request
+    try {
+      const fromUserId = req.user._id; // from the userAuth middleware the loggedIn user.
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
 
+      const allowedStatus = ["interested", "ignored"];
+      if (!allowedStatus.includes(status)) {
+        return res
+          .status(400)
+          .json({ message: "Invalid Status type: " + status });
+      }
+        
+      const connectionRequest = new ConnectionRequestModel({
+        fromUserId,
+        toUserId,
+        status, 
+      });
 
-requestRouter.post("/request/send/interested/:toUserId", userAuth, async (req, res) => {
-  //sending connection request
-  try{
-  const fromUserId = req.user._id;
+      const data = await connectionRequest.save(); // this will save it to the DB!
 
-  res.send(user.firstName + " send the connection request!!");
-  }catch(err){
-    res.status(400).send("ERROR: " + err.message);
+      res.json({
+        message: "Connection Request Sent Successfully!",
+        data,
+      });
+    } catch (err) {
+      res.status(400).send("ERROR: " + err.message);
+    }
   }
-});
-
-
+);
 
 module.exports = requestRouter;
