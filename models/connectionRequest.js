c8331697e75df9cba458019c17958f3cc8b8eb41
theflashyash99 <theflashyash99@gmail.com>
2025-12-08 15:13:41 +0530

    Sending Connection Request!

--- models/connectionRequest.js
+++ models/connectionRequest.js
@@ -1,35 +1,46 @@
 const mongoose = require("mongoose");
 
-const connectionRequestSchema = new mongoose.Schema({
-  fromUserId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    // ref: "User",
-  },
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      // ref: "User",
+    },
 
-  toUserId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    // ref: "User",
-  },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      // ref: "User",
+    },
 
-  status: {
-    type: String,
-    required:true,
-    enum: {
-      values: ["interested", "ignored", "accepted", "rejected"],   
-      message: `{value} is an incorrect status type`,
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["interested", "ignored", "accepted", "rejected"],
+        message: `{value} is an incorrect status type`,
+      },
     },
   },
-},
-{
-    timestamps:true,
+  {
+    timestamps: true,
+  }
+);
 
+connectionRequestSchema.pre("save", function (next) {
+  const connectionRequest = this
+  if (connectionRequest.fromUserId.equals(this.toUserId)) {
+    throw new Error("You Can't Send Request To Yourself!!");
+  }
+  next();
 });
 
-const ConnectionRequestModel =  new mongoose.model("ConnectionRequest", connectionRequestSchema);
+const ConnectionRequestModel = new mongoose.model(
+  "ConnectionRequest",
+  connectionRequestSchema
+);
 
-module.exports ={
-    ConnectionRequestModel,
-
-}
+module.exports = {
+  ConnectionRequestModel,
+};
