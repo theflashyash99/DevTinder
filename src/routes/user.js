24f9e1a02875909ad8bfa4a9a97fee181fbfc78d
theflashyash99 <theflashyash99@gmail.com>
2025-12-14 15:43:52 +0530

    Sending Connection Request!

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,13 +9,21 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
 
     const connectionRequest = await ConnectionRequestModel.find({
       toUserId: loggedInUser._id, // to find the see the connection request as the toUser is the one who got the request
-      status :  "interested",                
-    }).populate("fromUserId",["firstName","lastName"])  ;
+      status: "interested",
+    }).populate("fromUserId", ["firstName", "lastName"]);
 
     res.json({
-        message:"Data fetched successfully",
-        data: connectionRequest,
-    })
+      message: "Data fetched successfully",
+      data: connectionRequest,
+    });
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
   } catch (err) {
     res.status(400).send("Error: " + err.message);
   }
