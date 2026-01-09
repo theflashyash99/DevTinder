761ea2b9dbfc4ec9877b747f5c0a145bbc755f66
theflashyash99 <theflashyash99@gmail.com>
2026-01-09 14:36:58 +0530

    create cors injection

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const { userAuth } = require("./middleware/auth");
+const cors = require('cors')
+
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
