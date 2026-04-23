d0010d6a3c76ee746e5060b11092d0e0a565c7b0
theflashyash99 <theflashyash99@gmail.com>
2026-04-23 11:37:33 +0530

    deplyed socket

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -5,7 +5,7 @@ const { userAuth } = require("../middleware/auth");
 const initializeSocket = (server) => {
   const io = socket(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: ["http://localhost:5173","https://devtinder-pivz.onrender.com"] 
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
       allowedHeaders: ["Content-Type", "Authorization"],
       credentials: true,
